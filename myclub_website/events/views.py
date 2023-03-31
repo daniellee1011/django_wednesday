4205e1aa88e5be391790bba8d05fcbb492c326e2
daniellee1011 <kauvoi1011@gmail.com>
2023-03-31 00:40:32 -0400

    [Django] Create Starter template and Navbar
    : 1. Adopt Strater template(base.html)
    2. Create Navbar(navbar.html)
    
    https://getbootstrap.com/docs/5.3/getting-started/javascript/

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    now = datetime.now()
+    current_year = now.year
+
+    return render(request, 'events/home.html', {"current_year": current_year})
