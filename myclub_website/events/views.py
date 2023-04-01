56b2b52d0c4710679fd0bb778481be0b72b83d40
daniellee1011 <kauvoi1011@gmail.com>
2023-04-01 19:06:14 -0400

    [Django] Create Event page(event_list.html)
    : Create Event page with cards
    https://getbootstrap.com/docs/5.3/components/card/

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from datetime import datetime
+from .models import Event
 
 # Create your views here.
-def home(request):
+def all_events(request):
+    event_list = Event.objects.all()
+    return render(request, 'events/event_list.html',
+                  {'event_list': event_list})
+
+def home(request, year = datetime.now().year, month = datetime.now().month):
     now = datetime.now()
     current_year = now.year
 
