8d3cbf1a0f1843c95cdf13811eaf71402041aff0
daniellee1011 <kauvoi1011@gmail.com>
2023-07-17 15:43:26 -0400

    [Django] Create an add event page
    : https://www.youtube.com/watch?v=x99hW1N8Nug&list=PLCC34OHNcOtqW9BJmgQPPzUpJ8hl49AGy&index=11&t=1s

--- myclub_website/events/forms.py
+++ myclub_website/events/forms.py
@@ -1,12 +1,40 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Venue
+from .models import Venue, Event
+
+# Create an event form
+
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('name', 'event_date', 'venue',
+                  'manager', 'attendees', 'description')
+        labels = {
+            'name': '',
+            'event_date': 'YYYY-MM-DD HH:MM:SS',
+            'venue': 'Venue',
+            'manager': 'Manager',
+            'attendees': 'Attendees',
+            'description': '',
+        }
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Name'}),
+            'event_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Date'}),
+            'venue': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Venue'}),
+            'manager': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Manager'}),
+            'attendees': forms.SelectMultiple(attrs={'class': 'form-control', 'placeholder': 'Attendees'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+        }
 
 # Create a venue form
+
+
 class VenueForm(ModelForm):
     class Meta:
         model = Venue
-        fields = ('name', 'address', 'zip_code', 'phone', 'web', 'email_address')
+        fields = ('name', 'address', 'zip_code',
+                  'phone', 'web', 'email_address')
         labels = {
             'name': 'Enter your Venue here',
             'address': '',
@@ -16,10 +44,10 @@ class VenueForm(ModelForm):
             'email_address': '',
         }
         widgets = {
-            'name':  forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Venue name'}),
-            'address': forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Address'}),
-            'zip_code': forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Zip code'}),
-            'phone': forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Phone'}),
-            'web': forms.TextInput(attrs = {'class':'form-control', 'placeholder': 'Web address'}),
-            'email_address': forms.EmailInput(attrs = {'class':'form-control', 'placeholder': 'Email'}),
+            'name':  forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Venue Name'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zip Code'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
+            'web': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Web Address'}),
+            'email_address': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
         }
--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -2,37 +2,62 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 from django.http import HttpResponseRedirect
 from .models import Event, Venue
-from .forms import VenueForm
+from .forms import VenueForm, EventForm
 
 # Create your views here.
+
+
+def add_event(request):
+    print('add_event function called')
+    submitted = False
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_event?submitted=True')
+    else:
+        try:
+            form = EventForm()
+        except Exception as e:
+            print(e)
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'events/add_event.html', {'form': form, 'submitted': submitted})
+
+
 def update_venue(request, venue_id):
     venue = Venue.objects.get(pk=venue_id)
     form = VenueForm(request.POST or None, instance=venue)
     if form.is_valid():
-      form.save()
-      return redirect('list-venues')
+        form.save()
+        return redirect('list-venues')
 
     return render(request, 'events/update_venue.html', {'venue': venue, 'form': form})
+
 
 def search_venues(request):
     if request.method == "POST":
         # searched = request.POST['searched']
         # searched = request.POST('searched')
         searched = request.POST.get('searched')
-        venues = Venue.objects.filter(name__contains = searched)
+        venues = Venue.objects.filter(name__contains=searched)
         return render(request, 'events/search_venues.html', {'searched': searched, 'venues': venues})
     else:
         return render(request, 'events/search_venues.html', {})
 
+
 def show_venue(request, venue_id):
-    venue = Venue.objects.get(pk = venue_id)
+    venue = Venue.objects.get(pk=venue_id)
     return render(request, 'events/show_venue.html', {'venue': venue})
+
 
 def list_venues(request):
     venue_list = Venue.objects.all()
     return render(request, 'events/venue.html', {'venue_list': venue_list})
 
+
 def add_venue(request):
+    print('add_venue function called')
     submitted = False
     if request.method == "POST":
         form = VenueForm(request.POST)
@@ -43,15 +68,17 @@ def add_venue(request):
         form = VenueForm
         if 'submitted' in request.GET:
             submitted = True
-    
+
     return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted})
+
 
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'events/event_list.html',
                   {'event_list': event_list})
 
-def home(request, year = datetime.now().year, month = datetime.now().month):
+
+def home(request, year=datetime.now().year, month=datetime.now().month):
     now = datetime.now()
     current_year = now.year
 
